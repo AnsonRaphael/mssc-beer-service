8d93c7f3975bf3ac5e737f896f7c4c13a1a11fed
AnsonRaphael <anson550220@gmail.com>
2021-12-04 23:48:39 +0530

    added beer jpa

---
+++ src/main/java/com/nanos/msscbeerservice/domain/Beer.java
@@ -0,0 +1,39 @@
+package com.nanos.msscbeerservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name="UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 30,columnDefinition = "varchar",nullable = false,updatable = false)
+    private UUID beerId;
+    @Version
+    private Long version;
+    @CreationTimestamp
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerStyle;
+    private String beerName;
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
