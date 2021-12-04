109b08b675944e358048c454a1c0ee51cb225208
AnsonRaphael <anson550220@gmail.com>
2021-12-04 15:13:50 +0530

    initial commit

---
+++ src/test/java/com/nanos/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanos.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
