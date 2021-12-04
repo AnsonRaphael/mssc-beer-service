8eaa6a5bf2a2a560feaf3417ce83a0f76f0420eb
AnsonRaphael <anson550220@gmail.com>
2021-12-04 23:52:54 +0530

    added beer repository

---
+++ src/main/java/com/nanos/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.nanos.msscbeerservice.repositories;
+
+import com.nanos.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
